cb5996c721e4935bc3f909bd2c58bc71536d3ade
Kimtree <namook1308@gmail.com>
2018-12-08 15:23:48 +0900

    WordDB.py 작성, 단어DB file 추가
    WordDB.py
    - 파일 읽기
    - 랜덤 단어 반환

--- WordDB.py
+++ WordDB.py
@@ -1,1 +1,26 @@
-import pygame
+
+import random
+
+class FileHandler:
+
+    def __init__(self):
+        self.answer = ''
+
+        #read 'WordDB.txt' file
+        self.words = []
+        f = open('wordDB.txt', 'r')
+        lines = f.readlines()
+        f.close()
+
+        self.count = 0
+        for line in lines:
+            word = line.rstrip()
+            self.words.append(word)
+            self.count += 1
+
+        print("%d words in DB" %self.count)
+
+    def randFromDB(self):
+        r = random.randrange(self.count)
+        self.answer = self.words[r]
+        return self.answer
